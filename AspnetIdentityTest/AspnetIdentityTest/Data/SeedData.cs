ad0fc9deb9ebfb3be263ce7f063688c9f9f6d6f1
Shahrukh <shahrukh_hp1@hotmail.com>
2020-10-12 22:21:36 +0500

    initialize

---
+++ AspnetIdentityTest/AspnetIdentityTest/Data/SeedData.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetIdentityTest.Data
+{
+    public class SeedData : ISeedData
+    {
+        private ApplicationDbContext _context;
+
+        public SeedData(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async void SeedAdminUser()
+        {
+            var user = new IdentityUser
+            {
+                UserName = "admin",
+                NormalizedUserName = "admin",
+                Email = "admin@email.com",
+                NormalizedEmail = "admin@email.com",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString()
+            };
+
+            var roleStore = new RoleStore<IdentityRole>(_context);
+
+            if (!_context.Roles.Any(r => r.Name == "admin"))
+            {
+                await roleStore.CreateAsync(new IdentityRole { Name = "admin", NormalizedName = "admin" });
+                await roleStore.CreateAsync(new IdentityRole { Name = "company", NormalizedName = "company" });
+            }
+
+            if (!_context.Users.Any(u => u.UserName == user.UserName))
+            {
+                var password = new PasswordHasher<IdentityUser>();
+                var hashed = password.HashPassword(user, "123456");
+                user.PasswordHash = hashed;
+                var userStore = new UserStore<IdentityUser>(_context);
+                await userStore.CreateAsync(user);
+                await userStore.AddToRoleAsync(user, "admin");
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
